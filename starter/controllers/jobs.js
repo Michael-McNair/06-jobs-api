2b1fd4ee4b36efb721cca3dc380e07b2ea928f17
Michael McNair <michaeltsuyoshi@gmail.com>
2023-04-14 15:16:55 +0900

    initialize jobs controller

---
+++ starter/controllers/jobs.js
@@ -0,0 +1,17 @@
+const getAllJobs = async (req, res) => {
+  res.send('register user');
+};
+const getJob = async (req, res) => {
+  res.send('register user');
+};
+const createJob = async (req, res) => {
+  res.send('register user');
+};
+const updateJob = async (req, res) => {
+  res.send('register user');
+};
+const deleteJob = async (req, res) => {
+  res.send('register user');
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
