0ad5a3d3c2427e654f7b0be8e12a42d5098c6a30
michael mcnair <michaeltsuyoshi@gmail.com>
2023-04-18 22:32:06 +0900

    implement create jobs controller

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -1,12 +1,21 @@
+const Job = require('../models/Job');
+const { StatusCode } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
+
 const getAllJobs = async (req, res) => {
   res.send('register user');
 };
 const getJob = async (req, res) => {
   res.send('register user');
 };
+
 const createJob = async (req, res) => {
-  res.send('register user');
+  req.body.createdBy = req.user.userId;
+
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send('register user');
 };
